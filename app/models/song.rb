bb8da62dba2c0570083d306105414dd25abb8d73
Dani Lyn <dlyn1110@gmail.com>
2018-10-31 18:17:59 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genres
+  has_many :notes
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name)
+  end
+
+  def genre_name
+    self.genre.name
 end
